0f10ffbe89cad1b72b43db520b8af46fb94715f7
fusteraquiles-cyber <fusteraquiles@gmail.com>
2026-05-12 19:24:34 -0300

    Añade proyecto de consola básico en .NET 8.0
    Se incorpora el proyecto "Consola" a la solución, incluyendo su archivo Consola.csproj configurado para .NET 8.0. Se añade Program.cs con lógica para variables, impresión de parámetros y cálculo de edad a partir del año de nacimiento introducido por el usuario.

---
+++ Consola/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+public class Program
+{
+
+    private static void Main(string[] args)
+    {
+        CreamosVariables();
+       Console.WriteLine("Rojo puto");
+        ImpresionDeParametros(args);
+        CapturaValoresDelUsuario();
+    }
+
+    private static void CapturaValoresDelUsuario()
+    {
+        Console.Write("Escriba el año de su nacimiento: ");
+        string Nacimiento = Console.ReadLine();
+        int anio= Convert.ToInt32(Nacimiento);
+        int edad = DateTime.Now.Year - anio;
+        Console.WriteLine("Tu edad puede ser: " + edad);
+    }
+
+    private static void ImpresionDeParametros(string[] args)
+    {
+        if (args.Length > 1)
+            Console.WriteLine("hoal " + args[0] + " " + args[1]);
+    }
+
+    private static void CreamosVariables()
+    {
+        //creamos una variable int y le asignamos un valor
+        int numero = 10;
+        //declaracion
+        int numero2;
+        //asignacion
+        numero2 = 20;
+        int numero3 = 30;
+
+    }
+}
